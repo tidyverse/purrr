ca244b0996ff3293c06649aba4224d9be9e13ed2
hadley <h.wickham@gmail.com>
2014-11-29 11:50:47 -0600

    Add pluck

---
+++ R/pluck.R
@@ -0,0 +1,19 @@
+#' Extract elements of a list by position.
+#'
+#' @inheritParams map
+#' @param .i A string or integer index.
+#' @export
+#' @examples
+#' by_cyl <- split(mtcars, mtcars$cyl)
+#' mod_sum <- by_cyl %>%
+#'   map(lm, formula = mpg ~ wt) %>%
+#'   map(summary)
+#'
+#' # If .type is missing, you always get a list
+#' mod_sum %>% pluck("r.squared")
+#'
+#' # If you supply .type, you'll get a vector
+#' mod_sum %>% pluck("r.squared", numeric(1))
+pluck <- function(.x, .i, .type) {
+  map(.x, .subset2, .i, .type = .type)
+}
