d2ae585479200d5443d14dc3c337eeda2601a85b
hadley <h.wickham@gmail.com>
2014-12-01 09:10:38 -0600

    Implement flatten.
    Closes #6

---
+++ R/flatten.R
@@ -0,0 +1,19 @@
+#' Flatten a list of lists into a list.
+#'
+#' This is a wrapper for \code{\link{unlist}()} that defaults to
+#' \code{recursive = FALSE}.
+#'
+#' @param x A list of flatten
+#' @param recursive Recursively flatten list components?
+#' @export
+#' @examples
+#' x <- rerun(10, sample(5))
+#' x %>% flatten(x)
+#'
+#' # You can use flatten in conjunction with map
+#' x %>% map(~ x[1]) %>% flatten()
+#' # But it's better to specify the .type argument
+#' x %>% map(~ x[1], .type = numeric(1))
+flatten <- function(x, recursive = FALSE) {
+  unlist(x, recursive = recursive)
+}
