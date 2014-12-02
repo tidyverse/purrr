0d7e829480fc4230c689e7a14e25ef6b3e4dc178
hadley <h.wickham@gmail.com>
2014-12-02 09:49:06 -0600

    Tweaks to detect & detect_index.
    Better no match value for detect_index (0). Add .right argument to search from right.

--- R/find-position.R
+++ R/find-position.R
@@ -2,18 +2,26 @@
 #'
 #' @inheritParams every
 #' @inheritParams map
+#' @param .right If \code{FALSE}, the default, starts at the beginnging
+#'   of the vector and move towards the end; if \code{TRUE}, starts at the end
+#'   of the vector and moves towards the beginning.
 #' @return \code{detect} the value of the first item that matches the
 #'  predicate; \code{detect_index} the position of the matching item.
+#'  If not found, \code{detect} returns \code{NULL} and \code{detect_index}
+#'  returns 0.
 #' @export
 #' @examples
 #' is_even <- function(x) x %% 2 == 0
 #'
-#' 1:6 %>% detect(is_even)
-#' 1:6 %>% detect_index(is_even)
-detect <- function(.x, .f, ...) {
+#' 3:10 %>% detect(is_even)
+#' 3:10 %>% detect_index(is_even)
+#'
+#' 3:10 %>% detect(is_even, .right = TRUE)
+#' 3:10 %>% detect_index(is_even, .right = TRUE)
+detect <- function(.x, .f, ..., .right = FALSE) {
   .f <- as_function(.f)
 
-  for (i in seq_along(.x)) {
+  for (i in index(.x, .right)) {
     if (isTRUE(.f(.x[[i]], ...))) return(.x[[i]])
   }
   NULL
@@ -21,13 +29,20 @@ detect <- function(.x, .f, ...) {
 
 #' @export
 #' @rdname detect
-detect_index <- function(.x, .f, ...) {
+detect_index <- function(.x, .f, ..., .right = FALSE) {
   .f <- as_function(.f)
 
-  for (i in seq_along(.x)) {
+  for (i in index(.x, .right)) {
     if (isTRUE(.f(.x[[i]], ...))) return(i)
   }
-  NA_integer_
+  0
+}
+
+index <- function(x, right = FALSE) {
+  idx <- seq_along(x)
+  if (right)
+    idx <- rev(idx)
+  idx
 }
 
 #' Does a list contain an object?
