5e8764a957f96f210093a5451e4ce00accf55f1c
Lionel Henry <lionel.hry@gmail.com>
2014-12-18 18:44:24 +0100

    better default output

--- R/cross.R
+++ R/cross.R
@@ -2,10 +2,14 @@
 #'
 #' \code{cross} returns the product set of the elements of
 #' \code{.x}. It is similar to \code{expand.grid} but it takes and
-#' returns a list.
+#' returns a list and the output is more amenable to mapping
+#' operations.
 #'
 #' @seealso expand.grid
 #' @param .x A list.
+#' @param .wide If \code{TRUE}, returns a list of unique combinations.
+#'   If \code{FALSE}, returns a list of the same size as \code{.x}
+#'   (this format is equivalent to \code{as.list(expand.grid(.x))}).
 #' @export
 #' @examples
 #' # We build all combinations of names, greetings and separators from our
@@ -15,35 +19,43 @@
 #'   greeting = c("Hello.", "Bonjour."),
 #'   sep = c("! ", "... ")
 #' )
-#' args <- cross(data)
+#'
+#' cross(data) %>%
+#'   map(splat(paste))
+#'
+#' # This is equivalent to:
+#' args <- cross(data, .wide = FALSE)
 #'
 #' out <- vector("list", length(args[[1]]))
 #' for (i in seq_along(out))
 #'   out[[i]] <- map(args, i) %>% splat(paste)()
 #' out
-cross <- function(.x) {
+cross <- function(.x, .wide = TRUE) {
   remaining <- prod(vapply(.x, length, numeric(1)))
 
   if (remaining == 0) {
-    list()
+    return(list())
+  }
 
+  out <- vector("list", length(.x))
+  names(out) <- names(.x)
+  rep_factor <- 1
+
+  for (i in seq_along(.x)) {
+    x <- .x[[i]]
+    x_length <- length(x)
+    remaining <- remaining / x_length
+
+    seq <- rep.int(seq_len(x_length), rep.int(rep_factor, x_length))
+    seq <- rep.int(seq, remaining)
+    rep_factor <- rep_factor * x_length
+
+    out[[i]] <- x[seq]
+  }
+
+  if (.wide) {
+    unzip(out) %>% map(as.list)
   } else {
-    out <- vector("list", length(.x))
-    names(out) <- names(.x)
-    rep_factor <- 1
-
-    for (i in seq_along(.x)) {
-      x <- .x[[i]]
-      x_length <- length(x)
-      remaining <- remaining / x_length
-
-      seq <- rep.int(seq_len(x_length), rep.int(rep_factor, x_length))
-      seq <- rep.int(seq, remaining)
-      rep_factor <- rep_factor * x_length
-
-      out[[i]] <- x[seq]
-    }
-
     out
   }
 }
