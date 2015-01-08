d9eb631c74b201a22678bfd4342308bb930e8a39
Lionel Henry <lionel.hry@gmail.com>
2015-01-08 21:21:21 +0100

    Let splat() specify default arguments for .f

--- R/splat.R
+++ R/splat.R
@@ -1,11 +1,14 @@
 #' `Splat' arguments to a function.
 #'
 #' Wraps a function in \code{\link{do.call}()}, so instead of taking multiple
-#' arguments, it takes a single named list which will be interpreted as its arguments.
-#' This is useful when you want to pass a function a row of data frame or
-#' list, and don't want to manually pull it apart in your function.
+#' arguments, it takes a single named list which will be interpreted
+#' as its arguments.  This is useful when you want to pass a function
+#' a row of data frame or list, and don't want to manually pull it
+#' apart in your function. You can also specify default arguments for
+#' \code{.f} in the call to \code{splat()}.
 #'
 #' @param .f Function to splat.
+#' @param ... Default arguments for \code{.f}.
 #' @return A function.
 #' @export
 #' @examples
@@ -14,10 +17,16 @@
 #'
 #' # Or in a pipe:
 #' mean %>% splat() %>% invoke(x)
-splat <- function (.f) {
+#'
+#' # With functions that take a variable number of main arguments,
+#' # like \code{paste()}, it is often easier to specify default
+#' # arguments directly in \code{splat()}.
+#' c("01", "01", "2001") %>% splat(paste, sep = "-")()
+splat <- function (.f, ...) {
   force(.f)
+  defaults <- list(...)
   function(args = list()) {
-    do.call(.f, args)
+    do.call(.f, c(args, defaults))
   }
 }
 
