387418fb3da78639a63bb0dadb16fe1541edc038
Lionel Henry <lionel.hry@gmail.com>
2015-01-10 09:57:49 +0100

    Tests for data frame coercion

---
+++ tests/testthat/test-map.R
@@ -0,0 +1,30 @@
+context("map")
+
+test_that("map() returns a data frame when given one", {
+  df <- data.frame(a = c("a", "b"), b = c("a", "b"))
+
+  out1 <- map(df, as.character)
+  out2 <- map(df, `[[`, 1)
+
+  expect_is(out1, "data.frame")
+  expect_is(out2, "data.frame")
+})
+
+test_that("map2() and map3() return a data frame when given one", {
+  df <- data.frame(a = c("a", "b"), b = c("a", "b"))
+
+  out_map2 <- map2(df, names(df), function(x, y) paste(x, y))
+  out_map3 <- map3(df, names(df), c("1", "2"), function(x, y, z) paste(x, y, z))
+
+  expect_is(out_map2, "data.frame")
+  expect_is(out_map3, "data.frame")
+})
+
+test_that("map_n() returns a data frame when given one?", {
+  skip("Not sure yet what should happen")
+  df <- data.frame(a = c("a", "b"), b = c("a", "b"))
+
+  out <- map_n(df, paste)
+
+  expect_is(out, "data.frame? list?")
+})
