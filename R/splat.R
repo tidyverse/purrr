ba13fd3186bffbfb64c448d20386ac325991cfb5
Lionel Henry <lionel.hry@gmail.com>
2015-01-13 17:17:23 +0100

    Use enlist() instead of c() in splat()

--- R/splat.R
+++ R/splat.R
@@ -11,6 +11,7 @@
 #' @param ... Default arguments for \code{.f}. These will be evaluated
 #' once when \code{splat()} is called.
 #' @return A function.
+#' @seealso map_call do.call
 #' @export
 #' @examples
 #' x <- list(x = c(1:100, NA, 1000), na.rm = TRUE, trim = 0.9)
@@ -19,15 +20,16 @@
 #' # Or in a pipe:
 #' mean %>% splat() %>% invoke(x)
 #'
-#' # With functions that take a variable number of main arguments,
-#' # like paste(), it is often easier to specify default arguments
-#' # directly in splat().
-#' c("01", "01", "2001") %>% splat(paste, sep = "-")()
+#' # Default arguments can also be specified directly in splat()
+#' c(1:100, NA, 1000) %>% splat(mean, na.rm = TRUE)()
+#'
+#' # Note that when a splatted function gets a vector, it is wrapped
+#' # in a list. The above is equivalent to
+#' list(c(1:100, NA, 1000)) %>% splat(mean, na.rm = TRUE)()
 splat <- function (.f, ...) {
   force(.f)
   defaults <- list(...)
   function(args = list()) {
-    do.call(".f", c(args, defaults))
+    do.call(".f", enlist(args, defaults))
   }
 }
-
