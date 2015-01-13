995001d462ff3112a6da907ce50d4ebd6afbf754
Lionel Henry <lionel.hry@gmail.com>
2015-01-13 17:41:56 +0100

    Rename splat() to smash()

--- R/smash.R
+++ R/smash.R
@@ -1,32 +1,32 @@
-#' `Splat' arguments to a function.
+#' `Smash' a function open to make it take a list of arguments.
 #'
 #' Wraps a function in \code{\link{do.call}()}, so instead of taking
 #' multiple arguments, it takes a single named list which will be
 #' interpreted as its arguments. This is useful when you want to pass
 #' a function a row of data frame or list, and don't want to manually
 #' pull it apart in your function. You can also specify default
-#' arguments for \code{.f} in the call to \code{splat()}.
+#' arguments for \code{.f} in the call to \code{smash()}.
 #'
-#' @param .f Function to splat.
+#' @param .f Function to smash.
 #' @param ... Default arguments for \code{.f}. These will be evaluated
-#' once when \code{splat()} is called.
+#' once when \code{smash()} is called.
 #' @return A function.
 #' @seealso map_call do.call
 #' @export
 #' @examples
 #' x <- list(x = c(1:100, NA, 1000), na.rm = TRUE, trim = 0.9)
-#' splat(mean)(x)
+#' smash(mean)(x)
 #'
 #' # Or in a pipe:
-#' mean %>% splat() %>% invoke(x)
+#' mean %>% smash() %>% invoke(x)
 #'
-#' # Default arguments can also be specified directly in splat()
-#' c(1:100, NA, 1000) %>% splat(mean, na.rm = TRUE)()
+#' # Default arguments can also be specified directly in smash()
+#' c(1:100, NA, 1000) %>% smash(mean, na.rm = TRUE)()
 #'
-#' # Note that when a splatted function gets a vector, it is wrapped
+#' # Note that when a smashed function gets a vector, it is wrapped
 #' # in a list. The above is equivalent to
-#' list(c(1:100, NA, 1000)) %>% splat(mean, na.rm = TRUE)()
-splat <- function (.f, ...) {
+#' list(c(1:100, NA, 1000)) %>% smash(mean, na.rm = TRUE)()
+smash <- function (.f, ...) {
   force(.f)
   defaults <- list(...)
   function(args = list()) {
@@ -36,7 +36,7 @@ splat <- function (.f, ...) {
 
 #' Map a list to a function call
 #'
-#' While `splat()` wraps a function in \code{\link{do.call}()},
+#' While `smash()` wraps a function in \code{\link{do.call}()},
 #' `map_call()` is directly equivalent to \code{do.call()} except that
 #' it takes a list as first argument instead of a function. This makes
 #' `map_call()` pipable.
@@ -45,7 +45,7 @@ splat <- function (.f, ...) {
 #' @param .f A function or the name of a function to call with the
 #'   elements of \code{.x} as arguments.
 #' @param ... Additional arguments passed on to \code{.f}.
-#' @seealso splat do.call
+#' @seealso smash do.call
 #' @export
 #' @examples
 #' # We map a list of numbers to paste(), with sep = "-" as additional
