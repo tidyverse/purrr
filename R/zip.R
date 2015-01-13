598c8c94af7a5976630eaca962b22d9100d452cd
Lionel Henry <lionel.hry@gmail.com>
2015-01-14 00:24:01 +0100

    Follow .arg convention in unzip()

--- R/zip.R
+++ R/zip.R
@@ -37,20 +37,20 @@ zip <- function(.x) {
 unzip <- function(.x, .fields = NULL, .simplify = TRUE) {
   if (length(.x) == 0) return(list())
 
-  if (is.null(fields)) {
+  if (is.null(.fields)) {
     if (is.null(names(.x[[1]]))) {
-      fields <- seq_along(.x[[1]])
+      .fields <- seq_along(.x[[1]])
     } else {
-      fields <- setNames(names(.x[[1]]), names(.x[[1]]))
+      .fields <- setNames(names(.x[[1]]), names(.x[[1]]))
     }
   } else {
-    if (is.character(fields) && is.null(names(fields))) {
-      names(fields) <- fields
+    if (is.character(.fields) && is.null(names(.fields))) {
+      names(.fields) <- .fields
     }
   }
 
-  out <- lapply(fields, function(i) lapply(.x, .subset2, i))
-  if (simplify) out <- lapply(out, simplify_if_possible)
+  out <- lapply(.fields, function(i) lapply(.x, .subset2, i))
+  if (.simplify) out <- lapply(out, simplify_if_possible)
   out
 }
 
