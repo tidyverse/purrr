7a90040a5349379434ac86299cc23c79736e9948
Lionel Henry <lionel.hry@gmail.com>
2015-01-14 00:54:12 +0100

    Fix check() complaint in test

--- tests/testthat/test-cross.R
+++ tests/testthat/test-cross.R
@@ -4,7 +4,8 @@ test_that("long format corresponds to expand.grid output", {
   x <- data.frame(a = 1:3, b = 4:9)
 
   out1 <- cross_n(x)
-  out2 <- expand.grid(x, KEEP.OUT.ATTRS = FALSE) %>% as_data_frame()
+  out2 <- expand.grid(x, KEEP.OUT.ATTRS = FALSE) %>%
+    dplyr::as_data_frame()
 
   expect_equal(out1, out2)
 })
