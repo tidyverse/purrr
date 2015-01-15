5dc903975a9b8e34eb525ce448431a947fa973c5
Lionel Henry <lionel.hry@gmail.com>
2015-01-15 19:50:14 +0100

    Add prepend()

--- R/objects.R
+++ R/objects.R
@@ -27,6 +27,35 @@ enlist <- function(...) {
   dots %>% flatten() %>% setNames(names)
 }
 
+
+#' Prepend a vector
+#'
+#' This is a companion to \code{\link{append}()} to help merging two
+#' lists or atomic vectors.
+#'
+#' @param x the vector to be modified.
+#' @param values to be included in the modified vector.
+#' @param before a subscript, before which the values are to be appended.
+#' @return A merged vector.
+#' @export
+#' @examples
+#' x <- as.list(1:3)
+#'
+#' x %>% append("a")
+#' x %>% prepend("a")
+#' x %>% prepend(list("a", "b"), before = 3)
+prepend <- function(x, values, before = 1) {
+  n <- length(x)
+  stopifnot(before > 0 && before <= n)
+
+  if (before == 1) {
+    c(values, x)
+  } else {
+    c(x[1:(before - 1)], values, x[before:n])
+  }
+}
+
+
 #' Bare type predicates
 #'
 #' These predicates check for a given type but only return \code{TRUE}
