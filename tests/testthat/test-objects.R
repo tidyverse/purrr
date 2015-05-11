3a51278327f53457edd38f76f513cf354b26da3b
Lionel Henry <lionel.hry@gmail.com>
2015-05-11 21:25:10 +0200

    Rename enlist() to splice()

--- tests/testthat/test-objects.R
+++ tests/testthat/test-objects.R
@@ -1,12 +1,11 @@
 context("objects")
 
-test_that("enlist() produces correctly named lists", {
+test_that("splice() produces correctly named lists", {
   inputs <- list(arg1 = "a", arg2 = "b")
-  
-  out1 <- enlist(inputs, arg3 = c("c1", "c2")) %>% names()
-  out2 <- enlist(inputs, arg = list(arg3 = 1, arg4 = 2)) %>% names()
+
+  out1 <- splice(inputs, arg3 = c("c1", "c2")) %>% names()
+  out2 <- splice(inputs, arg = list(arg3 = 1, arg4 = 2)) %>% names()
 
   expect_equal(out1, c("arg1", "arg2", "arg3"))
   expect_equal(out2, c("arg1", "arg2", "arg3", "arg4"))
 })
-
