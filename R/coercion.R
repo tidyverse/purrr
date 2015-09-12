f4d5261f2eae6d8a6d0713b49cf8dc1537f6ef25
Lionel Henry <lionel.hry@gmail.com>
2015-09-12 12:19:38 +0200

    Mention difference with vapply() in as_vector() doc

--- R/coercion.R
+++ R/coercion.R
@@ -24,6 +24,10 @@
 #' # Vector molds are more flexible because they also specify the
 #' # length of the concatenated vectors:
 #' list(1:2, 3:4, 5:6) %>% as_vector(integer(2))
+#'
+#' # Note that unlike vapply(), as_vector() never adds dimension
+#' # attributes. So when you specify a vector mold of size > 1, you
+#' # always get a vector and not a matrix
 as_vector <- function(.x, .type) {
   if (can_simplify(.x, .type)) {
     unlist(.x)
