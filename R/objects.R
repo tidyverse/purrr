79518c8d86e06167bbf4fc0f3f9ab723cbf4761e
Lionel Henry <lionel.hry@gmail.com>
2015-09-15 02:44:03 +0200

    Add scalar type predicates

--- R/objects.R
+++ R/objects.R
@@ -197,6 +197,63 @@ is_logical <- function(x) {
   typeof(x) == "logical"
 }
 
+#' Scalar type predicates
+#'
+#' These predicates check for a given type and whether the vector is
+#' "scalar", that is, of length 1.
+#' @param x object to be tested.
+#' @seealso type-predicates bare-type-predicates
+#' @name scalar-type-predicates
+NULL
+
+#' @export
+#' @rdname scalar-type-predicates
+is_scalar_list <- function(x) {
+  is_list(x) && length(x) == 1
+}
+
+#' @export
+#' @rdname scalar-type-predicates
+is_scalar_atomic <- function(x) {
+  is_atomic(x) && length(x) == 1
+}
+
+#' @export
+#' @rdname scalar-type-predicates
+is_scalar_vector <- function(x) {
+  is_vector(x) && length(x) == 1
+}
+
+#' @export
+#' @rdname scalar-type-predicates
+is_scalar_numeric <- function(x) {
+  is_numeric(x) && length(x) == 1
+}
+
+#' @export
+#' @rdname scalar-type-predicates
+is_scalar_integer <- function(x) {
+  is_integer(x) && length(x) == 1
+}
+
+#' @export
+#' @rdname scalar-type-predicates
+is_scalar_double <- function(x) {
+  is_double(x) && length(x) == 1
+}
+
+#' @export
+#' @rdname scalar-type-predicates
+is_scalar_character <- function(x) {
+  is_character(x) && length(x) == 1
+}
+
+#' @export
+#' @rdname scalar-type-predicates
+is_scalar_logical <- function(x) {
+  is_logical(x) && length(x) == 1
+}
+
 #' Is a vector/list empty?
 #'
 #' @param x object to test
