07c2aa8d4c0fa9d9e277f1bbd774b39f778afd7b
hadley <h.wickham@gmail.com>
2017-03-05 20:30:39 +1300

    Refactor invoke tests

--- tests/testthat/test-invoke.R
+++ tests/testthat/test-invoke.R
@@ -1,5 +1,27 @@
 context("invoke")
 
+
+# invoke ------------------------------------------------------------------
+
+test_that("invoke() evaluates expressions in the right environment", {
+  shadowed_object <- letters
+  shadowed_fun <- toupper
+  expect_equal(invoke("shadowed_fun", list(quote(shadowed_object))), toupper(letters))
+})
+
+test_that("invoke() follows promises to find the evaluation env", {
+  shadowed_object <- letters
+  shadowed_fun <- toupper
+  f1 <- function(x) {
+    f2 <- function(x) purrr::invoke(x, list(quote(shadowed_object)))
+    f2(x)
+  }
+  environment(f1) <- baseenv()
+  expect_equal(f1("shadowed_fun"), toupper(letters))
+})
+
+# invoke_map --------------------------------------------------------------
+
 test_that("invoke_map() works with bare function", {
   data <- list(1:2, 3:4)
 
@@ -13,25 +35,8 @@ test_that("invoke_map() works with bare function", {
   expect_identical(invoke_map_lgl(`&&`, data), c(TRUE, TRUE))
 })
 
-test_that("invoke() evaluates expressions in the right environment", {
-  shadowed_object <- letters
-  shadowed_fun <- toupper
-  expect_equal(invoke("shadowed_fun", list(quote(shadowed_object))), toupper(letters))
-})
-
 test_that("invoke_map() evaluates expressions in the right environment", {
   shadowed_object <- letters
   shadowed_fun <- toupper
   expect_equal(invoke_map("shadowed_fun", list(quote(shadowed_object))), list(toupper(letters)))
-})
-
-test_that("invoke() follows promises to find the evaluation env", {
-  shadowed_object <- letters
-  shadowed_fun <- toupper
-  f1 <- function(x) {
-    f2 <- function(x) purrr::invoke(x, list(quote(shadowed_object)))
-    f2(x)
-  }
-  environment(f1) <- baseenv()
-  expect_equal(f1("shadowed_fun"), toupper(letters))
 })
