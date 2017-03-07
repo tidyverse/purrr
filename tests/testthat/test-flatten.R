7b1e99ebfed8642136e368f58d11ea731b5f4498
hadley <h.wickham@gmail.com>
2017-03-08 08:52:21 +1300

    Improve flatten tests

--- tests/testthat/test-flatten.R
+++ tests/testthat/test-flatten.R
@@ -4,13 +4,21 @@ test_that("input must be a list", {
   expect_error(flatten(1), "must be a list")
 })
 
-test_that("contents of list must be vectors", {
+test_that("contents of list must be supported types", {
   expect_error(flatten(list(quote(a))), "not a vector")
+  expect_error(flatten(list(expression(a))), "Unsupported type")
 })
 
 test_that("each second level element becomes first level element", {
   expect_equal(flatten(list(1:2)), list(1, 2))
   expect_equal(flatten(list(1, 2)), list(1, 2))
+})
+
+test_that("can flatten all atomic vectors", {
+  expect_equal(flatten(list(F)), list(F))
+  expect_equal(flatten(list(1L)), list(1L))
+  expect_equal(flatten(list(1)), list(1))
+  expect_equal(flatten(list("a")), list("a"))
 })
 
 test_that("NULLs are silently dropped", {
@@ -39,3 +47,25 @@ test_that("child names beat parent names", {
   out <- flatten(list(a = list(x = 1), b = list(y = 2)))
   expect_equal(out, list(x = 1, y = 2))
 })
+
+
+# atomic flatten ----------------------------------------------------------
+
+test_that("must be a list", {
+  expect_error(flatten_lgl(1), "must be a list")
+})
+
+test_that("can flatten all atomic vectors", {
+  expect_equal(flatten_lgl(list(F)), F)
+  expect_equal(flatten_int(list(1L)), 1L)
+  expect_equal(flatten_dbl(list(1)), 1)
+  expect_equal(flatten_chr(list("a")), "a")
+})
+
+test_that("preserves inner names", {
+  expect_equal(
+    flatten_dbl(list(c(a = 1), c(b = 2))),
+    c(a = 1, b = 2)
+  )
+})
+
