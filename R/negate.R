64a50e08d23a8c32756981d19b6b70f01f36e37e
Lionel Henry <lionel.hry@gmail.com>
2017-03-13 19:07:12 +0000

    Simplify negate() implementation

--- R/negate.R
+++ R/negate.R
@@ -14,33 +14,11 @@
 #' # Same as
 #' x %>% discard("y") %>% length()
 negate <- function(.p, .default = FALSE) {
-  .p <- tidy_capture(.p)
-  transform_expr(.p, ~ lang_interp({!(UQE(.))}), default = .default)
-}
+  .p <- as_mapper(.p)
 
-transform_expr <- function(quo_f, trans, default) {
-  trans <- rlang::as_function(trans)
+  body(.p) <- tidy_interp(quote({
+    ! { !!! body(.p) }
+  }))
 
-  f <- tidy_eval(quo_f)
-  if (is.function(f)) {
-    args <- fn_fmls(f)
-    call_args <- map(names(args), as_symbol)
-
-    body <- lang_interp(UQE(quo_f)(UQS(call_args)))
-  } else if (is_formula(f)) {
-    args <- f_mapper_args()
-    body <- f_rhs(quo_f)
-  } else if (is.character(f) || is.numeric(f) || is.list(f)) {
-    x <- NULL # quiet R CMD check warning
-    args <- alist(x = )
-    body <- lang_interp(extract(x, UQ(as.list(f)), default = UQ(default)))
-  } else {
-    stop("Unknown input", call. = FALSE)
-  }
-
-  new_fn(args, trans(body), f_env(quo_f))
-}
-
-lang_interp <- function(x) {
-  f_rhs(tidy_capture(x))
+  .p
 }
