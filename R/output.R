ed4622ce3890511001959d816e337de741b656b0
Richard Cotton <richierocks@gmail.com>
2017-06-11 17:55:07 -0400

    Add `insistently()`
    With help from @ijlyttle

--- R/output.R
+++ R/output.R
@@ -9,7 +9,13 @@
 #' @param quiet Hide errors (`TRUE`, the default), or display them
 #'   as they occur?
 #' @param otherwise Default value to use when an error occurs.
-#'
+#' @param max_attempts Positive integer. Functions wrapped by `insistently()`
+#'   will try to run this many times before giving up.
+#' @param wait_seconds Positive number. Base multiplier for time in seconds to
+#'   wait between attempts. The time increases exponentially, with a wait time
+#'   randomly chosen from a uniform distribution between `0` and
+#'   `wait_seconds * 2 ^ (i - 1)` seconds, between the `i`th and
+#'   `i + 1`th attempts.
 #' @return `safely`: wrapped function instead returns a list with
 #'   components `result` and `error`. If an error occurred, `error` is
 #'   an `error` object and `result` has a default value (`otherwise`).
@@ -21,6 +27,10 @@
 #'   `possibly`: wrapped function uses a default value (`otherwise`)
 #'   whenever an error occurs.
 #'
+#'   `insistently`: wrapped function attempts to run `max_attempts`
+#'    times; an error occurs if all attempts fail.
+#' @seealso [httr::RETRY()] is a special case of [insistently()]
+#' for HTTP verbs.
 #' @export
 #' @examples
 #' safe_log <- safely(log)
@@ -42,6 +52,31 @@
 #' # To replace errors with a default value, use possibly().
 #' list("a", 10, 100) %>%
 #'   map_dbl(possibly(log, NA_real_))
+#'
+#' # insistently() makes a function repeatedly try to work
+#' risky_runif <- function(lo = 0, hi = 1) {
+#'   y <- runif(1, lo, hi)
+#'   if(y < 0.9) {
+#'     stop(y, " is too small")
+#'   }
+#'   y
+#' }
+#' insistent_risky_runif <- insistently(
+#'   risky_runif, quiet = FALSE, wait_seconds = 0.01)
+#' set.seed(1)
+#' insistent_risky_runif()
+#' set.seed(3)
+#' try(insistent_risky_runif())
+#'
+#' # insistently() and possibly() are a useful combination
+#' possibly_insistent_risky_runif <- possibly(
+#'   insistently(risky_runif, quiet = FALSE, wait_seconds = 0.01),
+#'   otherwise = -99)
+#'
+#' set.seed(1)
+#' possibly_insistent_risky_runif()
+#' set.seed(3)
+#' possibly_insistent_risky_runif()
 #'
 #' # For interactive usage, auto_browse() is useful because it automatically
 #' # starts a browser() in the right place.
@@ -89,6 +124,41 @@ possibly <- function(.f, otherwise, quiet = TRUE) {
         stop("Terminated by user", call. = FALSE)
       }
     )
+  }
+}
+
+#' @export
+#' @rdname safely
+insistently <- function(.f, quiet = TRUE, max_attempts = 5, wait_seconds = 0.1) {
+  .f <- purrr::as_mapper(.f)
+
+  force(quiet)
+  force(max_attempts)
+  force(wait_seconds)
+
+  function(...) {
+    for (i in seq_len(max_attempts)) {
+      answer <- capture_error(.f(...), quiet = quiet)
+      if (is.null(answer$error)) {
+        return(answer$result)
+      }
+      if (wait_seconds > 0) {
+        actual_wait_seconds <- stats::runif(1, 0, wait_seconds * 2 ^ (i - 1))
+        if (!quiet) {
+          message(sprintf("Retrying in %.3g seconds.", actual_wait_seconds))
+        }
+        Sys.sleep(actual_wait_seconds)
+      }
+    }
+    if (!quiet) {
+      msg <- sprintf(
+        "%s failed after %d tries",
+        deparse(match.call()),
+        max_attempts
+      )
+      message(msg)
+    }
+    stop(answer$error)
   }
 }
 
