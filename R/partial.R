a843a0d53ae14e049e4f123c787386eabc5b6d50
Eugene Ha <eha@posteo.de>
2017-08-06 16:09:52 +0200

    Amend documentation

--- R/partial.R
+++ R/partial.R
@@ -12,18 +12,26 @@
 #' similar as possible to the anonymous functions that you'd create by hand,
 #' if you weren't using `partial`.
 #'
-#' @param ...f a function. For the output source to read well, this should be a
-#'   named function.
+#' @param ...f a function.
 #' @param ... named arguments to `...f` that should be partially applied.
-#' @param .env the environment of the created function. Defaults to
-#'   [parent.frame()] and you should rarely need to modify this.
-#' @param .lazy If `TRUE` arguments evaluated lazily, if `FALSE`,
-#'   evaluated when `partial` is called.
+#'   [Unquoting and splicing][rlang::UQ()] semantics are supported, see
+#'   _Examples_.
+#' @param .env Deprecated, see _Details_.
+#' @param .lazy Deprecated, see _Details_.
 #' @param .first If `TRUE`, the partialized arguments are placed
 #'   to the front of the function signature. If `FALSE`, they are
 #'   moved to the back. Only useful to control position matching of
 #'   arguments when the partialized arguments are not named.
+#'
+#' @details Partialized arguments are captured as [quosures][rlang::quo()], and
+#'   are [tidily evaluated][rlang::eval_tidy()], when `...f` is partially
+#'   applied. (Therefore, the `.env` argument is obsolete.) To fix the value of
+#'   a partially applied argument at the point where `partial()` is called,
+#'   unquote it with the \code{\link[rlang:UQ]{!!}} operator. (Previously, lazy
+#'   evaluation was controlled by the `.lazy` flag.)
+#'
 #' @export
+#'
 #' @examples
 #' # Partial is designed to replace the use of anonymous functions for
 #' # filling in function arguments. Instead of:
@@ -32,9 +40,9 @@
 #' # we can write:
 #' compact2 <- partial(discard, .p = is.null)
 #'
-#' # and the generated source code is very similar to what we made by hand
-#' compact1
+#' # When printed, the partialized arguments and original function are shown:
 #' compact2
+#' compact1
 #'
 #' # Note that the evaluation occurs "lazily" so that arguments will be
 #' # repeatedly evaluated
@@ -43,14 +51,20 @@
 #' f()
 #' f()
 #'
-#' # You can override this by saying .lazy = FALSE
-#' f <- partial(runif, n = rpois(1, 5), .lazy = FALSE)
+#' # You can override this by unquoting:
+#' f <- partial(runif, n = !! rpois(1, 5))
 #' f
 #' f()
 #' f()
 #'
-#' # This also means that partial works fine with functions that do
-#' # non-standard evaluation
+#' # Partialized arguments can be spliced:
+#' args <- list(n = rpois(1, 5))
+#' f <- partial(runif, !!! args)
+#' f
+#' f()
+#' f()
+#'
+#' # partial() works fine with functions that do non-standard evaluation
 #' my_long_variable <- 1:10
 #' plot2 <- partial(plot, my_long_variable)
 #' plot2()
