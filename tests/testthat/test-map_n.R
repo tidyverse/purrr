5029860f91af8016d8fc766add6f8232db35ebf5
Romain Francois <romain@thinkr.fr>
2018-02-04 21:23:01 -0800

    test for pmap_raw

--- tests/testthat/test-map_n.R
+++ tests/testthat/test-map_n.R
@@ -44,6 +44,7 @@ test_that("outputs are suffixes have correct type", {
   x <- 1:3
   expect_is(pmap_lgl(list(x), is.numeric), "logical")
   expect_is(pmap_int(list(x), length), "integer")
+  expect_is(pmap_raw(list(x), as.raw), "raw")
   expect_is(pmap_dbl(list(x), mean), "numeric")
   expect_is(pmap_chr(list(x), paste), "character")
   expect_is(pmap_dfr(list(x), as.data.frame), "data.frame")
