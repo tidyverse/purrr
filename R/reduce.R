55efd35d0e09e69eb22208b4f615b49a89faaa20
Christophe Dervieux <christophe.dervieux@gmail.com>
2018-05-06 19:00:45 +0200

    add examples for reduce2_right

--- R/reduce.R
+++ R/reduce.R
@@ -29,6 +29,7 @@
 #' paste2 <- function(x, y, sep = ".") paste(x, y, sep = sep)
 #' letters[1:4] %>% reduce(paste2)
 #' letters[1:4] %>% reduce2(c("-", ".", "-"), paste2)
+#' letters[1:4] %>% reduce2_right(c("-", ".", "-"), paste2)
 #'
 #' samples <- rerun(2, sample(10, 5))
 #' samples
@@ -40,6 +41,12 @@
 #' x %>% reduce_right(c)
 #' # Equivalent to:
 #' x %>% rev() %>% reduce(c)
+#'
+#' y <- list(c(6, 7), c(8, 9))
+#' reduce2(x, y, paste)
+#' reduce2_right(x, y, paste)
+#' # Equivalent to:
+#' x %>% rev() %>% reduce2(rev(y), paste)
 reduce <- function(.x, .f, ..., .init) {
   reduce_impl(.x, .f, ..., .init = .init, .left = TRUE)
 }
