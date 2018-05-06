267271bf73107c92c7b3070a06ae122eb844faf8
Christophe Dervieux <christophe.dervieux@gmail.com>
2018-05-06 19:25:18 +0200

    get back historical flatmap() from 2f76d3e4

---
+++ R/map-vector.R
@@ -0,0 +1,38 @@
+#' Map a function and flatten the result by one-level
+#'
+#' \code{flatmap()} is equivalent to \code{map()} followed by
+#' \code{flatten()}. You can also provide \code{.type} to check the
+#' resulting type conforms to you expectations.
+#'
+#' Compared to \code{\link{map_lgl}()}, \code{\link{map_chr}()}, etc,
+#' \code{flatmap()} is adapted to functions returning a variable
+#' number of elements.
+#' @inheritParams map
+#' @param .type A string indicating which type you expect the results
+#'   of \code{.f} should be.
+#' @export
+#' @seealso \code{\link{map_lgl}()}, \code{\link{map_chr}()},
+#'   \code{\link{map_dbl}()}, \code{\link{map_int}()}
+#' @examples
+#' # Sample a variable number of elements from each column and
+#' # concatenate the results
+#' var_select <- function(x) sample(x, size = rdunif(1, 5))
+#' c(mtcars) %>% flatmap(var_select)
+#'
+#' # You can also check that the results are of expected type
+#' \dontrun{
+#' c(mtcars) %>% flatmap(var_select, .type = "character")
+#' }
+#' c(mtcars) %>% flatmap(var_select, .type = "numeric")
+flatmap <- function(.x, .f, ..., .type) {
+  out <- map(.x, .f = .f, ...)
+  if (!missing(.type)) {
+    if (!is_scalar_character(.type)) {
+      stop(".type must be a string")
+    }
+    if (!can_simplify(out, .type)) {
+      stop("Results do not conform to .type", call. = FALSE)
+    }
+  }
+  flatten(out)
+}
