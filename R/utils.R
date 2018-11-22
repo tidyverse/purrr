7938b3281c6268d6f5305f93ef9e5b4fb05a301a
Lionel Henry <lionel.hry@gmail.com>
2018-11-22 12:44:28 +0100

    Embed rlang deprecaters

--- R/utils.R
+++ R/utils.R
@@ -113,6 +113,10 @@ is_names <- function(nms) {
   is_character(nms) && !any(is.na(nms) | nms == "")
 }
 
+paste_line <- function(...) {
+  paste(chr(...), collapse = "\n")
+}
+
 # From rlang
 lifecycle <- function(stage) {
   url <- paste0("https://www.tidyverse.org/lifecycle/#", stage)
@@ -157,4 +161,61 @@ lifecycle_img <- function(stage, url) {
 upcase1 <- function(x) {
   substr(x, 1, 1) <- toupper(substr(x, 1, 1))
   x
+}
+
+# From rlang
+signal_soft_deprecated <- function(msg,
+                                   id = msg,
+                                   package = NULL,
+                                   env = caller_env(2)) {
+  if (is_true(peek_option("lifecycle_disable_verbose_retirement"))) {
+    return(invisible(NULL))
+  }
+
+  if (is_true(peek_option("lifecycle_force_verbose_retirement")) ||
+      is_reference(env, global_env())) {
+    warn_deprecated(msg, id)
+    return(invisible(NULL))
+  }
+
+  if (package_attached(package, caller_env())) {
+    warn_deprecated(msg, id)
+    return(invisible(NULL))
+  }
+
+  signal(msg, "lifecycle_soft_deprecated")
+}
+package_attached <- function(package, env) {
+  if (is_null(package)) {
+    top <- topenv(env)
+    if (!is_namespace(top)) {
+      abort("`signal_soft_deprecated()` must be called from a package function")
+    }
+    package <- ns_env_name(top)
+  } else {
+    stopifnot(is_string(package))
+  }
+
+  pkg_env_name(package) %in% search()
+}
+
+warn_deprecated <- function(msg, id = msg) {
+  if (is_true(peek_option("lifecycle_disable_verbose_retirement"))) {
+    return(invisible(NULL))
+  }
+  if (env_has(deprecation_env, id)) {
+    return(invisible(NULL))
+  }
+
+  env_poke(deprecation_env, id, TRUE);
+
+  .Deprecated(msg = paste_line(
+    msg,
+    silver("This warning is displayed once per session.")
+  ))
+}
+deprecation_env <- new.env(parent = emptyenv())
+
+abort_defunct <- function(msg) {
+  .Defunct(msg = msg)
 }
