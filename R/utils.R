e7c85ca7e41df3807e64320bc4163f91dad0cb4f
Lionel Henry <lionel.hry@gmail.com>
2018-11-22 10:53:42 +0100

    Add lifecycle badge infrastructure

--- R/utils.R
+++ R/utils.R
@@ -112,3 +112,49 @@ ndots <- function(...) nargs()
 is_names <- function(nms) {
   is_character(nms) && !any(is.na(nms) | nms == "")
 }
+
+# From rlang
+lifecycle <- function(stage) {
+  url <- paste0("https://www.tidyverse.org/lifecycle/#", stage)
+  img <- lifecycle_img(stage, url)
+
+  sprintf(
+    "\\ifelse{html}{%s}{\\strong{%s}}",
+    img,
+    upcase1(stage)
+  )
+}
+lifecycle_img <- function(stage, url) {
+  file <- sprintf("lifecycle-%s.svg", stage)
+  stage_alt <- upcase1(stage)
+
+  switch(stage,
+
+    experimental = ,
+    maturing = ,
+    stable = ,
+    questioning = ,
+    archived =
+      sprintf(
+        "\\out{<a href='%s'><img src='%s' alt='%s lifecycle'></a>}",
+        url,
+        file.path("figures", file),
+        stage_alt
+      )
+   ,
+
+    `soft-deprecated` = ,
+    deprecated = ,
+    defunct =
+      sprintf(
+        "\\figure{%s}{options: alt='%s lifecycle'}",
+        file,
+        stage_alt
+      )
+
+  )
+}
+upcase1 <- function(x) {
+  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+  x
+}
