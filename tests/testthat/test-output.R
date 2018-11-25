0cd44d2828e9fca68b260370b3f223c00af96f9d
James Lamb <jaylamb20@gmail.com>
2018-11-25 11:05:34 -0600

    Added test on uncovered line in possibly()

--- tests/testthat/test-output.R
+++ tests/testthat/test-output.R
@@ -37,6 +37,12 @@ test_that("possibly returns default value on failure", {
   expect_identical(possibly(log, NA_real_)("a"), NA_real_)
 })
 
+test_that("possibly emits a message on failure if quiet = FALSE", {
+  expect_message({
+    possibly(log, NA_real_, quiet = FALSE)("a")
+  }, regexp = "non-numeric argument to mathematical function")
+})
+
 test_that("auto_browse() not intended for primitive functions", {
   expect_error(auto_browse(log)(NULL), "primitive functions")
   expect_error(auto_browse(identity)(NULL), NA)
