5a6c10884aae367e603e685425f55bcfefc132b3
Lionel Henry <lionel.hry@gmail.com>
2018-12-10 11:13:59 +0100

    Don't use `expect_error()` to test for error construction
    See https://github.com/tidyverse/purrr/pull/598/files#r240005608

--- tests/testthat/test-conditions.R
+++ tests/testthat/test-conditions.R
@@ -10,17 +10,17 @@ test_that("stop_bad_type() stores fields", {
 })
 
 test_that("stop_bad_type() constructs default `what`", {
-  expect_error(
+  expect_error_cnd(
     stop_bad_type(NA, "`NULL`"),
     "Object must be `NULL`",
     "purrr_error_bad_type"
   )
-  expect_error(
+  expect_error_cnd(
     stop_bad_type(NA, "`NULL`", arg = ".foo"),
     "`.foo` must be `NULL`",
     "purrr_error_bad_type"
   )
-  expect_error(
+  expect_error_cnd(
     stop_bad_type(NA, "`NULL`", arg = quote(.foo)),
     "`arg` must be `NULL` or a string, not a symbol",
     "purrr_error_bad_type"
@@ -28,19 +28,19 @@ test_that("stop_bad_type() constructs default `what`", {
 })
 
 test_that("stop_bad_element_type() constructs type errors", {
-  expect_error(
+  expect_error_cnd(
     stop_bad_element_type(1:3, 3, "a foobaz"),
     "Element 3 must be a foobaz, not an integer vector",
     "purrr_error_bad_element_type"
   )
 
-  expect_error(
+  expect_error_cnd(
     stop_bad_element_type(1:3, 3, "a foobaz", actual = "a quux"),
     "Element 3 must be a foobaz, not a quux",
     "purrr_error_bad_element_type"
   )
 
-  expect_error(
+  expect_error_cnd(
     stop_bad_element_type(1:3, 3, "a foobaz", arg = "..arg"),
     "Element 3 of `..arg` must be a foobaz, not an integer vector",
     "purrr_error_bad_element_type"
@@ -48,7 +48,7 @@ test_that("stop_bad_element_type() constructs type errors", {
 })
 
 test_that("stop_bad_element_type() accepts `what`", {
-  expect_error(
+  expect_error_cnd(
     stop_bad_element_type(1:3, 3, "a foobaz", what = "Result"),
     "Result 3 must be a foobaz, not an integer vector",
     "purrr_error_bad_element_type"
@@ -64,27 +64,27 @@ test_that("stop_bad_length() stores fields", {
 })
 
 test_that("stop_bad_length() constructs error message", {
-  expect_error(stop_bad_length(1:3, 10), "Vector must have length 10, not 3")
-  expect_error(stop_bad_length(1:3, 10, arg = ".foo"), "`.foo` must have length 10, not 3")
-  expect_error(stop_bad_length(1:3, 10, arg = ".foo", what = "This thing"), "This thing must have length 10, not 3")
+  expect_error_cnd(stop_bad_length(1:3, 10), "Vector must have length 10, not 3", "purrr_error_bad_length")
+  expect_error_cnd(stop_bad_length(1:3, 10, arg = ".foo"), "`.foo` must have length 10, not 3", "purrr_error_bad_length")
+  expect_error_cnd(stop_bad_length(1:3, 10, arg = ".foo", what = "This thing"), "This thing must have length 10, not 3", "purrr_error_bad_length")
 })
 
 test_that("stop_bad_element_length() constructs error message", {
-  expect_error(stop_bad_element_length(1:3, 8, 10), "Element 8 must have length 10, not 3")
-  expect_error(stop_bad_element_length(1:3, 8, 10, arg = ".foo"), "Element 8 of `.foo` must have length 10, not 3")
-  expect_error(stop_bad_element_length(1:3, 8, 10, arg = ".foo", what = "Result"), "Result 8 of `.foo` must have length 10, not 3")
+  expect_error_cnd(stop_bad_element_length(1:3, 8, 10), "Element 8 must have length 10, not 3", "purrr_error_bad_element_length")
+  expect_error_cnd(stop_bad_element_length(1:3, 8, 10, arg = ".foo"), "Element 8 of `.foo` must have length 10, not 3", "purrr_error_bad_element_length")
+  expect_error_cnd(stop_bad_element_length(1:3, 8, 10, arg = ".foo", what = "Result"), "Result 8 of `.foo` must have length 10, not 3", "purrr_error_bad_element_length")
 })
 
 test_that("stop_bad_vector() constructs error message", {
-  expect_error(stop_bad_vector(1:3, character(), 1), "Vector must be a single string, not an integer vector of length 3")
-  expect_error(stop_bad_vector(factor(c("a", "b")), character(), 10), "Vector must be a character vector of length 10, not a vector of class `factor` and of length 2")
-  expect_error(stop_bad_vector(1:3, character(), 10, .recycle = TRUE), "Vector must be a character vector of length 1 or 10, not an integer vector of length 3")
-  expect_error(stop_bad_vector(1:3, 1:2, 10, what = "This foobaz vector", .recycle = TRUE), "This foobaz vector must be an integer vector of length 1 or 10, not an integer vector of length 3")
-  expect_error(stop_bad_vector(list(1, 2), logical(), 10, arg = ".quux", .recycle = TRUE), "`.quux` must be a logical vector of length 1 or 10, not a list of length 2")
+  expect_error_cnd(stop_bad_vector(1:3, character(), 1), "Vector must be a single string, not an integer vector of length 3", "purrr_error_bad_vector")
+  expect_error_cnd(stop_bad_vector(factor(c("a", "b")), character(), 10), "Vector must be a character vector of length 10, not a vector of class `factor` and of length 2", "purrr_error_bad_vector")
+  expect_error_cnd(stop_bad_vector(1:3, character(), 10, .recycle = TRUE), "Vector must be a character vector of length 1 or 10, not an integer vector of length 3", "purrr_error_bad_vector")
+  expect_error_cnd(stop_bad_vector(1:3, 1:2, 10, what = "This foobaz vector", .recycle = TRUE), "This foobaz vector must be an integer vector of length 1 or 10, not an integer vector of length 3", "purrr_error_bad_vector")
+  expect_error_cnd(stop_bad_vector(list(1, 2), logical(), 10, arg = ".quux", .recycle = TRUE), "`.quux` must be a logical vector of length 1 or 10, not a list of length 2", "purrr_error_bad_vector")
 })
 
 test_that("stop_bad_element_vector() constructs error message", {
-  expect_error(stop_bad_element_vector(1:3, 3, character(), 1), "Element 3 must be a single string, not an integer vector of length 3")
-  expect_error(stop_bad_element_vector(1:3, 20, 1:2, 10, what = "Result", .recycle = TRUE), "Result 20 must be an integer vector of length 1 or 10, not an integer vector of length 3")
-  expect_error(stop_bad_element_vector(list(1, 2), 1, logical(), 10, arg = ".quux", .recycle = TRUE), "Element 1 of `.quux` must be a logical vector of length 1 or 10, not a list of length 2")
+  expect_error_cnd(stop_bad_element_vector(1:3, 3, character(), 1), "Element 3 must be a single string, not an integer vector of length 3", "purrr_error_bad_element_vector")
+  expect_error_cnd(stop_bad_element_vector(1:3, 20, 1:2, 10, what = "Result", .recycle = TRUE), "Result 20 must be an integer vector of length 1 or 10, not an integer vector of length 3", "purrr_error_bad_element_vector")
+  expect_error_cnd(stop_bad_element_vector(list(1, 2), 1, logical(), 10, arg = ".quux", .recycle = TRUE), "Element 1 of `.quux` must be a logical vector of length 1 or 10, not a list of length 2", "purrr_error_bad_element_vector")
 })
