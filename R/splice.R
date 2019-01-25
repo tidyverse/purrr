3c8953fa9622fca8e6a015e8036bc644429a055e
Lionel Henry <lionel.hry@gmail.com>
2019-01-25 11:22:26 +0100

    Mark `splice()` as questioning

--- R/splice.R
+++ R/splice.R
@@ -1,11 +1,21 @@
 #' Splice objects and lists of objects into a list
 #'
+#' @description
+#'
+#' \Sexpr[results=rd, stage=render]{purrr:::lifecycle("questioning")}
+#'
 #' This splices all arguments into a list. Non-list objects and lists
 #' with a S3 class are encapsulated in a list before concatenation.
 #'
 #' @param ... Objects to concatenate.
 #' @return A list.
-#' @export
+#'
+#' @section Life cycle:
+#'
+#' `splice()` is in the questioning lifecycle stage as of purrr
+#' 0.3.0. We are now favouring the `!!!` syntax enabled by
+#' [rlang::list2()].
+#'
 #' @examples
 #' inputs <- list(arg1 = "a", arg2 = "b")
 #'
@@ -13,6 +23,7 @@
 #' splice(inputs, arg3 = c("c1", "c2")) %>% str()
 #' list(inputs, arg3 = c("c1", "c2")) %>% str()
 #' c(inputs, arg3 = c("c1", "c2")) %>% str()
+#' @export
 splice <- function(...) {
   splice_if(list(...), is_bare_list)
 }
