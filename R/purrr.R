d2107a8af0fa6cd3067f0a3cfe68ca90d400fd14
Lionel Henry <lionel.hry@gmail.com>
2019-02-27 16:37:36 +0100

    Force arguments in reducing functions
    Closes #643

--- R/purrr.R
+++ R/purrr.R
@@ -2,3 +2,12 @@
 #' @import rlang
 #' @useDynLib purrr, .registration = TRUE
 "_PACKAGE"
+
+
+has_force_and_call <- FALSE
+
+.onLoad <- function(lib, pkg) {
+  if (getRversion() >= "3.2.3") {
+    has_force_and_call <<- TRUE
+  }
+}
