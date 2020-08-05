74f6dd8597881e7a3be1805a91680a86cdaa034b
Lionel Henry <lionel.hry@gmail.com>
2020-08-05 10:41:07 +0200

    Use experimental vctrs C API

--- src/init.c
+++ src/init.c
@@ -7,6 +7,9 @@
 #include <R_ext/Visibility.h>
 #define export attribute_visible extern
 
+SEXP purrr_init_library(SEXP);
+
+
 /* .Call calls */
 extern SEXP coerce_impl(SEXP, SEXP);
 extern SEXP pluck_impl(SEXP, SEXP, SEXP, SEXP);
@@ -18,20 +21,30 @@ extern SEXP transpose_impl(SEXP, SEXP);
 extern SEXP vflatten_impl(SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"coerce_impl",    (DL_FUNC) &coerce_impl,    2},
-    {"pluck_impl",   (DL_FUNC) &pluck_impl,   4},
-    {"flatten_impl",   (DL_FUNC) &flatten_impl,   1},
-    {"map_impl",       (DL_FUNC) &map_impl,       4},
-    {"map2_impl",      (DL_FUNC) &map2_impl,      5},
-    {"pmap_impl",      (DL_FUNC) &pmap_impl,      4},
-    {"transpose_impl", (DL_FUNC) &transpose_impl, 2},
-    {"vflatten_impl",  (DL_FUNC) &vflatten_impl,  2},
-    {"purrr_eval",     (DL_FUNC) &Rf_eval,        2},
-    {NULL, NULL, 0}
+  {"purrr_init_library",    (DL_FUNC) &purrr_init_library, 1},
+  {"coerce_impl",           (DL_FUNC) &coerce_impl,    2},
+  {"pluck_impl",            (DL_FUNC) &pluck_impl,   4},
+  {"flatten_impl",          (DL_FUNC) &flatten_impl,   1},
+  {"map_impl",              (DL_FUNC) &map_impl,       4},
+  {"map2_impl",             (DL_FUNC) &map2_impl,      5},
+  {"pmap_impl",             (DL_FUNC) &pmap_impl,      4},
+  {"transpose_impl",        (DL_FUNC) &transpose_impl, 2},
+  {"vflatten_impl",         (DL_FUNC) &vflatten_impl,  2},
+  {"purrr_eval",            (DL_FUNC) &Rf_eval,        2},
+  {NULL, NULL, 0}
 };
 
 export void R_init_purrr(DllInfo *dll)
 {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
+}
+
+
+// From <vctrs.c>
+void vctrs_init_api();
+
+SEXP purrr_init_library(SEXP ns) {
+  vctrs_init_api();
+  return R_NilValue;
 }
