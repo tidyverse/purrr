186f21940fb5375036e8fcd6a98bd00a13d9c7a3
Hadley Wickham <h.wickham@gmail.com>
2022-08-29 13:56:48 -0500

    Update src/pluck.c
    Co-authored-by: Lionel Henry <lionel.hry@gmail.com>

--- src/pluck.c
+++ src/pluck.c
@@ -256,7 +256,7 @@ SEXP pluck_impl(SEXP x, SEXP index, SEXP missing, SEXP strict_arg) {
 
  end:
   UNPROTECT(1);
-  return Rf_isNull(x) ? missing : x;
+  return x == R_NilValue ? missing : x;
 }
 
 
