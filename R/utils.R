3de6c94757ac7377d2fe56a60b6459274b038e78
Hadley Wickham <h.wickham@gmail.com>
2022-08-31 14:11:37 -0500

    Switch from crayon to cli

--- R/utils.R
+++ R/utils.R
@@ -177,23 +177,6 @@ friendly_type_of_element <- function(x) {
 }
 
 
-has_crayon <- function() is_installed("crayon") && crayon::has_color()
-
-red       <- function(x) if (has_crayon()) crayon::red(x)       else x
-blue      <- function(x) if (has_crayon()) crayon::blue(x)      else x
-green     <- function(x) if (has_crayon()) crayon::green(x)     else x
-yellow    <- function(x) if (has_crayon()) crayon::yellow(x)    else x
-magenta   <- function(x) if (has_crayon()) crayon::magenta(x)   else x
-cyan      <- function(x) if (has_crayon()) crayon::cyan(x)      else x
-blurred   <- function(x) if (has_crayon()) crayon::blurred(x)   else x
-silver    <- function(x) if (has_crayon()) crayon::silver(x)    else x
-bold      <- function(x) if (has_crayon()) crayon::bold(x)      else x
-italic    <- function(x) if (has_crayon()) crayon::italic(x)    else x
-underline <- function(x) if (has_crayon()) crayon::underline(x) else x
-
-bullet <- function(...) paste0(bold(silver(" * ")), sprintf(...))
-
-
 vec_simplify <- function(x) {
   if (!vctrs::vec_is_list(x)) {
     return(x)
