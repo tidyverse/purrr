4a08bff528a0827c2e4cfdb9cf3e9476ff77ea85
Hadley Wickham <h.wickham@gmail.com>
2022-09-09 07:36:23 -0500

    Deprecate invoke_*() (#924)
    And remove map_call().
    
    Fixes #911

--- R/retired-invoke.R
+++ R/retired-invoke.R
@@ -1,15 +1,33 @@
 #' Invoke functions.
 #'
-#' @keywords internal
 #' @description
 #'
-#' `r lifecycle::badge("superseded")`
+#' `r lifecycle::badge("deprecated")`
 #'
-#' This pair of functions make it easier to combine a function and list
-#' of parameters to get a result. `invoke` is a wrapper around
-#' `do.call` that makes it easy to use in a pipe. `invoke_map`
-#' makes it easier to call lists of functions with lists of parameters.
+#' * `invoke()` is deprecated in favour of the simpler `exec()` function
+#'   reexported from rlang. `exec()` evaluates a function call built
+#'   from its inputs and supports [dynamic dots][rlang::dyn-dots]:
 #'
+#'   ```R
+#'   # Before:
+#'   invoke(mean, list(na.rm = TRUE), x = 1:10)
+#'
+#'   # After
+#'   exec(mean, 1:10, !!!list(na.rm = TRUE))
+#'   ```
+#'
+#' * `invoke_map()` is deprecated because it's harder to understand than the
+#'   corresponding code using `map()`/`map2()` and `exec()`:
+#'
+#'   ```R
+#'   # Before:
+#'   invoke_map(fns, list(args))
+#'   invoke_map(fns, list(args1, args2))
+#'
+#'   # After:
+#'   map(fns, exec, !!!args)
+#'   map2(fns, list(args1, args2), \(fn, args) exec(fn, !!!args))
+#'   ```
 #' @param .f For `invoke`, a function; for `invoke_map` a
 #'   list of functions.
 #' @param .x For `invoke`, an argument-list; for `invoke_map` a
@@ -22,86 +40,46 @@
 #'   evaluate a constructed expression. This only matters if you pass
 #'   as `.f` the name of a function rather than its value, or as
 #'   `.x` symbols of objects rather than their values.
-#' @section Life cycle:
-#'
-#' These functions are retired in favour of [exec()]. They are no
-#' longer under active development but we will maintain them in the
-#' package undefinitely.
-#'
-#' * `invoke()` is retired in favour of the simpler `exec()` function
-#'   reexported from rlang. `exec()` evaluates a function call built
-#'   from its inputs and supports [dynamic dots][rlang::dyn-dots]:
-#'
-#'   ```
-#'   # Before:
-#'   invoke(mean, list(na.rm = TRUE), x = 1:10)
-#'
-#'   # After
-#'   exec(mean, 1:10, !!!list(na.rm = TRUE))
-#'   ```
-#'
-#' * `invoke_map()` is is retired without replacement because it is
-#'   more complex to understand than the corresponding code using
-#'   `map()`, `map2()` and `exec()`:
-#'
-#'   ```
-#'   # Before:
-#'   invoke_map(fns, list(args))
-#'   invoke_map(fns, list(args1, args2))
-#'
-#'   # After:
-#'   map(fns, exec, !!!args)
-#'   map2(fns, list(args1, args2), function(fn, args) exec(fn, !!!args))
-#'   ```
-#'
-#' @family map variants
+#' @keywords internal
 #' @examples
-#' # Invoke a function with a list of arguments
+#' # was
 #' invoke(runif, list(n = 10))
-#' # Invoke a function with named arguments
 #' invoke(runif, n = 10)
+#' # now
+#' exec(runif, n = 10)
 #'
-#' # Combine the two:
-#' invoke(paste, list("01a", "01b"), sep = "-")
-#' # That's more natural as part of a pipeline:
-#' list("01a", "01b") %>%
-#'   invoke(paste, ., sep = "-")
+#' # was
+#' args <- list("01a", "01b")
+#' invoke(paste, args, sep = "-")
+#' # now
+#' exec(paste, !!!args, sep = "-")
 #'
-#' # Invoke a list of functions, each with different arguments
-#' invoke_map(list(runif, rnorm), list(list(n = 10), list(n = 5)))
-#' # Or with the same inputs:
-#' invoke_map(list(runif, rnorm), list(list(n = 5)))
-#' invoke_map(list(runif, rnorm), n = 5)
-#' # Or the same function with different inputs:
-#' invoke_map("runif", list(list(n = 5), list(n = 10)))
+#' # was
+#' funs <- list(runif, rnorm)
+#' funs %>% invoke_map(n = 5)
+#' funs %>% invoke_map(list(list(n = 10), list(n = 5)))
 #'
-#' # Or as a pipeline
+#' # now
+#' funs %>% map(exec, n = 5)
+#' funs %>% map2(list(list(n = 10), list(n = 5)), function(f, args) exec(f, !!!args))
+#'
+#' # or use pmap + a tibble
+#' df <- tibble::tibble(
+#'   fun = list(runif, rnorm),
+#'   args = list(list(n = 10), list(n = 5))
+#' )
+#' df %>% pmap(function(fun, args) exec(fun, !!!args))
+#'
+#'
+#' # was
 #' list(m1 = mean, m2 = median) %>% invoke_map(x = rcauchy(100))
-#' list(m1 = mean, m2 = median) %>% invoke_map_dbl(x = rcauchy(100))
+#' # now
+#' list(m1 = mean, m2 = median) %>% map(function(f) f(rcauchy(100)))
 #'
-#' # Note that you can also match by position by explicitly omitting `.x`.
-#' # This can be useful when the argument names of the functions are not
-#' # identical
-#' list(m1 = mean, m2 = median) %>%
-#'   invoke_map(, rcauchy(100))
-#'
-#' # If you have pairs of function name and arguments, it's natural
-#' # to store them in a data frame. Here we use a tibble because
-#' # it has better support for list-columns
-#' if (rlang::is_installed("tibble")) {
-#'   df <- tibble::tibble(
-#'     f = c("runif", "rpois", "rnorm"),
-#'     params = list(
-#'       list(n = 10),
-#'       list(n = 5, lambda = 10),
-#'       list(n = 10, mean = -3, sd = 10)
-#'     )
-#'   )
-#'   df
-#'   invoke_map(df$f, df$params)
-#' }
 #' @export
 invoke <- function(.f, .x = NULL, ..., .env = NULL) {
+  lifecycle::deprecate_warn("0.4.0", "invoke()", "exec()")
+
   .env <- .env %||% parent.frame()
   args <- c(as.list(.x), list(...))
   do.call(.f, args, envir = .env)
@@ -118,6 +96,8 @@ as_invoke_function <- function(f) {
 #' @rdname invoke
 #' @export
 invoke_map <- function(.f, .x = list(NULL), ..., .env = NULL) {
+  lifecycle::deprecate_warn("0.4.0", "invoke_map()", I("map() + exec()"))
+
   .env <- .env %||% parent.frame()
   .f <- as_invoke_function(.f)
   map2(.f, .x, invoke, ..., .env = .env)
@@ -125,6 +105,8 @@ invoke_map <- function(.f, .x = list(NULL), ..., .env = NULL) {
 #' @rdname invoke
 #' @export
 invoke_map_lgl <- function(.f, .x = list(NULL), ..., .env = NULL) {
+  lifecycle::deprecate_warn("0.4.0", "invoke_lgl()", I("map_lgl() + exec()"))
+
   .env <- .env %||% parent.frame()
   .f <- as_invoke_function(.f)
   map2_lgl(.f, .x, invoke, ..., .env = .env)
@@ -132,6 +114,8 @@ invoke_map_lgl <- function(.f, .x = list(NULL), ..., .env = NULL) {
 #' @rdname invoke
 #' @export
 invoke_map_int <- function(.f, .x = list(NULL), ..., .env = NULL) {
+  lifecycle::deprecate_warn("0.4.0", "invoke_int()", I("map_int() + exec()"))
+
   .env <- .env %||% parent.frame()
   .f <- as_invoke_function(.f)
   map2_int(.f, .x, invoke, ..., .env = .env)
@@ -139,6 +123,8 @@ invoke_map_int <- function(.f, .x = list(NULL), ..., .env = NULL) {
 #' @rdname invoke
 #' @export
 invoke_map_dbl <- function(.f, .x = list(NULL), ..., .env = NULL) {
+  lifecycle::deprecate_warn("0.4.0", "invoke_dbl()", I("map_dbl() + exec()"))
+
   .env <- .env %||% parent.frame()
   .f <- as_invoke_function(.f)
   map2_dbl(.f, .x, invoke, ..., .env = .env)
@@ -146,6 +132,8 @@ invoke_map_dbl <- function(.f, .x = list(NULL), ..., .env = NULL) {
 #' @rdname invoke
 #' @export
 invoke_map_chr <- function(.f, .x = list(NULL), ..., .env = NULL) {
+  lifecycle::deprecate_warn("0.4.0", "invoke_chr()", I("map_chr() + exec()"))
+
   .env <- .env %||% parent.frame()
   .f <- as_invoke_function(.f)
   map2_chr(.f, .x, invoke, ..., .env = .env)
@@ -153,14 +141,19 @@ invoke_map_chr <- function(.f, .x = list(NULL), ..., .env = NULL) {
 #' @rdname invoke
 #' @export
 invoke_map_raw <- function(.f, .x = list(NULL), ..., .env = NULL) {
+  lifecycle::deprecate_warn("0.4.0", "invoke_raw()", I("map_raw() + exec()"))
+
   .env <- .env %||% parent.frame()
   .f <- as_invoke_function(.f)
-  map2_raw(.f, .x, invoke, ..., .env = .env)
+
+  map2_raw_(.f, .x, invoke, ..., .env = .env)
 }
 
 #' @rdname invoke
 #' @export
 invoke_map_dfr <- function(.f, .x = list(NULL), ..., .env = NULL) {
+  lifecycle::deprecate_warn("0.4.0", "invoke_df()", I("map() + exec() + list_rbind()"))
+
   .env <- .env %||% parent.frame()
   .f <- as_invoke_function(.f)
   map2_dfr(.f, .x, invoke, ..., .env = .env)
@@ -168,6 +161,8 @@ invoke_map_dfr <- function(.f, .x = list(NULL), ..., .env = NULL) {
 #' @rdname invoke
 #' @export
 invoke_map_dfc <- function(.f, .x = list(NULL), ..., .env = NULL) {
+  lifecycle::deprecate_warn("0.4.0", "invoke_dfc()", I("map() + exec() + list_cbind()"))
+
   .env <- .env %||% parent.frame()
   .f <- as_invoke_function(.f)
   map2_dfc(.f, .x, invoke, ..., .env = .env)
@@ -175,11 +170,10 @@ invoke_map_dfc <- function(.f, .x = list(NULL), ..., .env = NULL) {
 #' @rdname invoke
 #' @export
 #' @usage NULL
-invoke_map_df <- invoke_map_dfr
+invoke_map_df <- function(.f, .x = list(NULL), ..., .env = NULL) {
+  lifecycle::deprecate_warn("0.4.0", "invoke_df()", I("map() + exec() + list_rbind()"))
 
-#' @rdname invoke
-#' @export
-#' @usage NULL
-map_call <- function(.x, .f, ...) {
-  .Defunct("`map_call()` is deprecated. Please use `invoke()` instead.")
+  .env <- .env %||% parent.frame()
+  .f <- as_invoke_function(.f)
+  map2_dfr(.f, .x, invoke, ..., .env = .env)
 }
