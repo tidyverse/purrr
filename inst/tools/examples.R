57c779a652dc9dfc90e3a8dd3c8a78945890a297
Hadley Wickham <h.wickham@gmail.com>
2022-09-14 16:05:33 -0500

    Remove ! used for testing

--- inst/tools/examples.R
+++ inst/tools/examples.R
@@ -2,7 +2,7 @@ if (getRversion() < "4.1") {
   dir.create("man/macros", showWarnings = FALSE, recursive = TRUE)
   cat(
     paste(
-      "\\renewcommand{\\examples}{\\section{Examples!}{",
+      "\\renewcommand{\\examples}{\\section{Examples}{",
       "These examples are designed to work in R >= 4.1 so that we can take",
       "advantage of modern syntax like the base pipe (\\verb{|>}) and the ",
       "function shorthand (\\verb{\\(x) x + 1}). They might not work on the ",
