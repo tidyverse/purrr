cd77a700a10bde3dbe9380f617a762d23b17a011
Lionel Henry <lionel.hry@gmail.com>
2022-09-27 22:30:21 +0200

    Pass user env to lifecycle
    To differentiate between direct and indirect calls

--- R/coerce.R
+++ R/coerce.R
@@ -10,11 +10,12 @@ coerce_dbl <- function(x) coerce(x, "double")
 coerce_chr <- function(x) coerce(x, "character")
 
 
-deprecate_to_char <- function(type) {
+deprecate_to_char <- function(type, call = caller_env()) {
   lifecycle::deprecate_warn(
     "1.0.0",
     I(paste0("Automatic coercion from ", type, " to character")),
     I("an explicit call to as.character() within map_chr()"),
-    always = TRUE
+    always = TRUE,
+    user_env = call
   )
 }
