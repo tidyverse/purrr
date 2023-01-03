2a4e12e9553829767c334217c3c8967e777e60bd
W. Kyle Hamilton <kyle.hamilton@gmail.com>
2023-01-03 13:20:35 -0800

    Correction of minor typos (#1028)

--- R/adverb-safely.R
+++ R/adverb-safely.R
@@ -14,7 +14,7 @@
 #'   * A named function, e.g. `mean`.
 #'   * An anonymous function, e.g. `\(x) x + 1` or `function(x) x + 1`.
 #'   * A formula, e.g. `~ .x + 1`. Only recommended if you require backward
-#'     compatibility with olders version of R.
+#'     compatibility with older versions of R.
 #' @param otherwise Default value to use when an error occurs.
 #' @param quiet Hide errors (`TRUE`, the default), or display them
 #'   as they occur?
