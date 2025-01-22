52b1b68f60a6a9b3d6e763aa1bbb2a0a221b5645
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-01-22 10:24:03 +0000

    re-export mirai::daemons()

--- R/package-purrr.R
+++ R/package-purrr.R
@@ -3,7 +3,11 @@
 #' @import vctrs
 #' @importFrom cli cli_progress_bar
 #' @importFrom lifecycle deprecated
+#' @importFrom mirai daemons
 #' @useDynLib purrr, .registration = TRUE
 "_PACKAGE"
 
 the <- new_environment()
+
+#' @export
+mirai::daemons
