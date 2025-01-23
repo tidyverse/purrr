765a6617b3b5254a8abf2a2dbfb4622789c508b6
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2025-01-23 11:34:11 +0000

    simplify wording in docs

--- R/parallelization.R
+++ R/parallelization.R
@@ -1,10 +1,9 @@
 #' Parallelization in purrr
 #'
 #' @description
-#' purrr's map functions have a `.parallel` argument that you can use to
-#' parallelize a map using the \CRANpkg{mirai} package. This allows you to run
-#' computations in parallel using more cores on your machine, or distributed
-#' over the network.
+#' purrr's map functions have a `.parallel` argument to parallelize a map using
+#' the \CRANpkg{mirai} package. This allows you to run computations in parallel
+#' using more cores on your machine, or distributed over the network.
 #'
 #' `.parallel` can be:
 #'
@@ -15,16 +14,16 @@
 #'
 #' ## Supplying objects to parallel processes
 #'
-#' In the majority of cases, such as when `.f` is a function from the base or a
-#' package namespace, it is possible to simply set `.parallel` to `TRUE`.
+#' In the majority of cases, such as when `.f` is a base or package function,
+#' simply set `.parallel` to `TRUE`.
 #'
 #' If `.f` is a user-defined function, then ideally everything required by the
 #' function is passed in as arguments (a pure function). Any constant arguments
 #' in this case can be supplied through `...` in the usual way.
 #'
 #' However, if the function references an object that is not supplied as an
-#' argument or defined in the function itself (a free variable), then these
-#' should be supplied as a list to `.parallel` so that they are available to
+#' argument or defined in the function itself, i.e. a free variable, these
+#' should be supplied as a list to `.parallel`. This makes them available to
 #' `.f` in the parallel processes. `purrr` requires these to be explicitly
 #' supplied, rather than try to determine them from analysing `.f`, as this
 #' provides for consistent and reliable behaviour that is hence easy to debug.
@@ -44,8 +43,8 @@
 #'
 #' How and where parallelization occurs is determined by
 #' [`daemons()`][mirai::daemons]. This is a function from the \pkg{mirai}
-#' package that sets up daemons, or persistent background processes, on your
-#' local machine or across the network.
+#' package that sets up daemons (persistent background processes that receive
+#' parallel computations) on your local machine or across the network.
 #'
 #' \pkg{purrr} requires these to be set up prior to performing any parallel map
 #' operations. If this is not done, you will see a warning from the \pkg{mirai}
@@ -55,15 +54,15 @@
 #'
 #' `daemons()`arguments:
 #'
-#' * `n`: specify an integer number to launch daemons on your local machine,
-#'   e.g. `daemons(7)`. As a rule of thumb, you want to leave at least one core
-#'   for the main R process, so, for maximum efficiency, this should be at most
-#'   one less than the number of cores on your machine.
+#' * `n`: the number of daemons to launch on your local machine, e.g.
+#'   `daemons(7)`. As a rule of thumb, for maximum efficiency this should be (at
+#'   most) one less than the number of cores on your machine, leaving one core
+#'   for the main R process.
 #' * `url` and `remote`: used to set up and launch daemons for distributed
 #'   computing over the network. See [daemons][mirai::daemons] function
 #'   documentation for more details.
-#' * None: calling `daemons()` with no arguments returns a short summary of
-#'   the current connection status and tasks queued and completed.
+#' * None: calling `daemons()` with no arguments returns a summary of the
+#'   current connection status and mirai tasks queued and completed.
 #'
 #' For more details, including on further arguments, see
 #' [daemons][mirai::daemons].
@@ -72,16 +71,17 @@
 #'
 #' `daemons(0)` resets and terminates any existing daemons.
 #'
-#' All daemons automatically terminate when there is no longer a connection to
-#' the main session e.g. when you end it. For this reason, it is not required to
-#' specifically terminate daemons in this instance, although it is still good
-#' practice to do so.
+#' All daemons automatically terminate when you end your session and there is no
+#' longer a connection. For this reason, it is not required to specifically
+#' terminate daemons in this instance, although it is still good practice to do
+#' so.
 #'
 #' ## with() method
 #'
-#' A statement may be evaluated with daemons set up for the duration required.
-#' The `with()` method for daemons provides a convenient way of doing this. As
-#' an example:
+#' The `with()` method for daemons provides a convenient way of evaluating a
+#' statement with daemons set up for the duration of the statement.
+#'
+#' As an example, the below is evaluated in parallel using 7 daemons:
 #'
 #' \preformatted{
 #' with(daemons(7), {
